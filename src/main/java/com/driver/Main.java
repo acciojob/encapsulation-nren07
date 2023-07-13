19663208c86586d4c17545dc9687f80abc0fd8b8
nren07 <imnren07@gmail.com>
2023-07-13 10:28:28 +0530

    try to pass testcases

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj=new RWOnly();
 //        obj.name="hello"; //name has private access in com.driver.RWOnly
-        obj.setter("name");
-        obj.getter();
+        obj.setName("name");
+        System.out.println(obj.getName());
     }
 }
