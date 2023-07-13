5d07655b48bdaf6b24f38197b7f48c47e96047cb
nren07 <imnren07@gmail.com>
2023-07-13 10:08:52 +0530

    all done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+//        obj.name="hello"; //name has private access in com.driver.RWOnly
+        obj.setters("Naren");
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private static String name;
+
+    public void setters(String name){
+         this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
 }
